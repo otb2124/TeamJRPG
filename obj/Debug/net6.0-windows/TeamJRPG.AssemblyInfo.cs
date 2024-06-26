db3493a380a4efb380da113a33207f95e7e243bf
otb2124 <otb2124@g.rit.edu>
2024-06-26 17:02:08 +0200

    updt

--- obj/Debug/net6.0-windows/TeamJRPG.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/TeamJRPG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeamJRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b92839e73dfdd54f2977390a7e90b7babdabd96a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b313a492311f41dff120921fbf463f9a7ee74ba7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeamJRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeamJRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
